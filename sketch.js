024767ae2007bc860e7d7c05b1da255f7732b219
SnehaPant <72789523+SnehaPant@users.noreply.github.com>
2020-11-01 07:33:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -41,7 +41,7 @@ function draw() {
 }
 function keyPressed(){
   if (keyCode===UP_ARROW){
-    Matter.Body.applyForce(papers.body,papers.body.position,{x:15,y:-1})
+    Matter.Body.applyForce(papers.body,papers.body.position,{x:85,y:-85})
   }
 
 }
